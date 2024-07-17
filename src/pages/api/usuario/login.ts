1e813ebcf74b90b4f637f875a3f822434b66a74d
Pablo Muñoz Villegas <pblvmunoz@gmail.com>
2024-07-16 23:06:50 -0400

    aaaa

--- src/pages/api/usuario/login.ts
+++ src/pages/api/usuario/login.ts
@@ -7,72 +7,52 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!process.env.JWT_SECRET) {
         return res.status(500).json({ error: 'JWT_SECRET not defined' });
     }
-
+    
     const JWT_SECRET = process.env.JWT_SECRET;
 
     if (req.method === 'POST') {
         const { email, contrasena } = req.body;
-
-        if (!email || !contrasena) {
-            return res.status(400).json({ error: 'Email y contraseña son obligatorios' });
-        }
-
+    
         try {
-            const user = await db.usuario.findUnique({
-                where: { email },
-                select: {
-                    id: true,
-                    nombre: true,
-                    apellido: true,
-                    email: true,
-                    numero: true,
-                    rol: true, // Incluye rol aquí
-                },
-            });
-
-            if (!user) {
-                return res.status(401).json({ error: 'Credenciales inválidas' });
-            }
-
-            // Busca el usuario completo para acceder a la contraseña
-            const userWithPassword = await db.usuario.findUnique({
-                where: { email },
-                select: {
-                    contrasena: true,
-                },
-            });
-
-            // Verifica si userWithPassword es null
-            if (!userWithPassword) {
-                return res.status(401).json({ error: 'Credenciales inválidas' });
-            }
-
-            const passwordMatch = await compare(contrasena, userWithPassword.contrasena);
-
-            if (!passwordMatch) {
-                return res.status(401).json({ error: 'Credenciales inválidas' });
-            }
-
-            const token = sign({ userId: user.id, rol: user.rol }, JWT_SECRET, {
-                expiresIn: '1h',
-            });
-
-            res.status(200).json({
-                token,
-                user: {
-                    id: user.id,
-                    nombre: user.nombre,
-                    apellido: user.apellido,
-                    email: user.email,
-                    numero: user.numero,
-                    rol: user.rol,
-                },
-            });
+          let user = await db.usuario.findFirst({
+            where: {
+              OR: [
+                { email: email },
+              ],
+            },
+          });
+    
+          if (!user) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+          }
+    
+          const passwordMatch = await compare(contrasena, user.contrasena);
+    
+          if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+          }
+    
+          const token = sign({ userId: user.id }, JWT_SECRET, {
+            expiresIn: '1h',
+          });
+    
+          res.status(200).json({
+            token,
+            user: {
+              id: user.id,
+              nombre: user.nombre,
+              apellido: user.apellido,
+              email: user.email,
+              numero: user.numero,
+              rol: user.rol,
+            },
+          });
         } catch (error) {
-            console.error('Login failed:', error);
-            res.status(500).json({ error: 'Error en el inicio de sesión' });
+          console.error('Login failed:', error);
+          res.status(500).json({ error: 'Error en el inicio de sesión' });
         }
-    } else {
+      } else {
         res.status(405).json({ error: 'Método no permitido' });
-    }
+      }
 }
+    
