da4eb95b6760e35fa9c9efca3b81f298a7334f8c
Pablo Muñoz Villegas <pblvmunoz@gmail.com>
2024-07-16 23:41:42 -0400

    a

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,10 +1,18 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+let db: PrismaClient;
 
-export const db = prisma
+if (process.env.NODE_ENV === 'production') {
+  db = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  db = global.prisma;
+}
+
+export { db };
