ab4ff7dfec8b9bae637fea412088ed9b3c94d2d4
Pablo Muñoz Villegas <pblvmunoz@gmail.com>
2024-07-17 08:51:44 -0400

    uwu

--- src/pages/api/producto/getProductos.ts
+++ src/pages/api/producto/getProductos.ts
@@ -1,10 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import {db} from "../../../lib/lib";
-import corsMiddleware from '../../../middleware/cors';
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await corsMiddleware(req, res);
     if (req.method === 'GET') {
         try {
             const productos = await db.productoVender.findMany();
